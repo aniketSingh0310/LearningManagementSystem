99a983460f3d4061df19770d817f8b36e29f35c1
aniketSingh0310 <aniketreuls@gmail.com>
2023-11-17 10:57:58 +0530

    course category form

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -4,7 +4,8 @@ import * as z from "zod";
 import axios from "axios";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {useForm} from "react-hook-form";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
 import {
     Form, FormControl, FormDescription, FormField,FormLabel,FormItem,FormMessage
@@ -27,11 +28,17 @@ const form= useForm<z.infer<typeof formSchema>>({
         title:""
     },
 });
-
+const router = useRouter();
 const {isSubmitting, isValid}= form.formState;
 
-const onSubmit=(values:z.infer<typeof formSchema>)=>{
-console.log(values);
+const onSubmit= async (values:z.infer<typeof formSchema>)=>{
+  try {
+    const response = await axios.post("/api/courses", values);
+    router.push(`/teacher/courses/${response.data.id}`);
+    toast.success("Course created");
+  } catch {
+    toast.error("Something went wrong");
+  }
 
 }
     return ( 
